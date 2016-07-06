da60ea33e1716cbc07bc7bb4814fd538e2cc7090
Robert Prine <robertprine2@yahoo.com>
2016-07-05 23:11:57 -0400

    Added intro and started on logic to split up equation into digestable bites

--- src/Calculator/Program.cs
+++ src/Calculator/Program.cs
@@ -7,8 +7,49 @@ namespace Calculator
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Enter an equation to solve or 'exit' to quit.");
+
+            string problem = Console.ReadLine();
+
+            if (problem.Equals("exit", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            else
+            {
+                Calculate(problem);
+                return true;
+            }
+        }
+
+        public static void Calculate(string problem)
+        {
+            if (problem.Contains("+"))
+            {
+                int location = problem.IndexOf("+");
+                string a = problem.Substring(0, location);
+                string b = problem.Substring(location + 1);
+                int aNumber = ToInt32(a);
+                Console.WriteLine(b);
+                Console.ReadLine();
+            }
+        }
+
+        public static void Addition()
+        {
+
         }
     }
 }
